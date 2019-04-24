40e67e4a82f218a8b9585a331a77b587ccb503d6
dennisly <dgranadosl@ucenfotec.ac.cr>
2019-04-23 21:26:41 -0600

    Se agrega la clase persistencia para la serializacion y guardado de los parrafos.

---
+++ InMemoryEditor/src/PersistenciaTexto.java
@@ -0,0 +1,38 @@
+import java.util.List;
+
+import com.google.gson.Gson;
+
+public class PersistenciaTexto {
+
+	private Gson jsonSerializer = new Gson();
+	
+	public List<Paragraph> retornarParrafosGuardados(){
+		
+		return null;
+		
+	}
+	
+	public List<Memento> retornarEstadosAnterioresGuardados(){
+		return null;
+	}
+	
+	
+	public String serializarListaParrafos() {
+		String jsonLista = "";
+		
+		return jsonLista;
+	}
+	
+	
+	public String serializarListaEstadosAnterioresParrafos() {
+		String jsonLista = "";
+		
+		return jsonLista;
+	}
+	
+	public void guardarTextoListas() {
+		
+	}
+	
+	
+}
